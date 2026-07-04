a492342e9e3876260621055618ea560b55890708
ecofinsouharda <ecofinsouharda@gmail.com>
2026-07-04 14:49:28 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,12 +5,65 @@ document
 .onclick = () => {
 
     password.type =
-        password.type === "password"
-        ? "text"
-        : "password";
+    password.type === "password"
+    ? "text"
+    : "password";
 
 };
 
 document
 .getElementById("loginBtn")
-.onclick = login;
+.onclick = async function(){
+
+    const collectorId =
+    document
+    .getElementById("collectorId")
+    .value
+    .trim();
+
+    const password =
+    document
+    .getElementById("password")
+    .value;
+
+    const status =
+    document
+    .getElementById("status");
+
+    status.innerHTML="Logging in...";
+
+    try{
+
+        const result =
+        await api(
+            "login",
+            {
+                collectorId,
+                password
+            }
+        );
+
+        if(!result.success){
+
+            status.innerHTML=result.message;
+
+            return;
+
+        }
+
+        localStorage.setItem(
+            "collector",
+            JSON.stringify(result.data)
+        );
+
+        window.location="dashboard.html";
+
+    }
+
+    catch(e){
+
+        status.innerHTML=e.message;
+
+    }
+
+};
